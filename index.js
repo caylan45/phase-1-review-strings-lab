ce4fc4fff0fd35471f9e74b8de74f19d5f9a0d79
caylan wilcox <caylan45@yahoo.com>
2022-11-02 11:33:34 -0500

    commit tree e7e1fb5e903c4f80e4f9e0a64283cbd1b6a84a16

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser=`HELLO`
+let welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage=`WELCOME TO FLATBOOK, ${currentUser}!`;
+let shortGreeting = `Welcome, ${currentUser[0]}!`
